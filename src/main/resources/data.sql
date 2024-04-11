349b5a0b178f4d747f7469634ca29e18b02f72ff
spiderX00 <re.difelice@gmail.com>
2024-04-11 17:37:05 +0200

    Upload data.sql to import the sql data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,13 +11,3 @@ INSERT INTO "books" ("id", "isbncode", "author", "date_added", "date_removed", "
 (4,	'872093644-3',	'Inglis Conelly',	'2022-12-24 00:00:00',	NULL,	4,	'',	'Manatee'),
 (11, '037789243-2', 'Katha Drewe', '11/1/2022', '4/22/2023', 11, '', 'Dove, little brown'),
 (12, '042096613-7', 'Robinia Issett', '3/8/2023', '1/4/2024', 12, '', 'Madagascar hawk owl');
-13, '077806761-0', 'Filia Sarfas', '8/19/2022', '4/30/2024', 13, '', 'Giant anteater')
-(14, '961962187-5', 'Robin Waycot', '1/14/2022', '4/27/2023', 14, '', 'Mongoose, javan gold-spotted'),
-(15, '568584845-5', 'Adlai Durie', '4/8/2022', '1/16/2024', 15, '', 'Yellow baboon');
-(16, '224279920-7', 'Kendra Salerg', '7/2/2022', '8/28/2023', 16, '', 'Malagasy ground boa');
-(17, '420789113-7', 'Flossy Arkley', '4/28/2022', '9/3/2023', 17, '', 'Deer, roe');
-(18, '155961395-5', 'Vin Dunnet', '2/3/2022', '2/16/2024', 18, '', 'Hyrax');
-
-INSERT INTO "users" ("id", "email", "last_name", "name") VALUES
-(1,	'fabio.bianchi@gmail.com',	'Bianchi',	'Fabio'),
-(2,	'mario.rossi2@gmail.com',	'Rossi',	'Mario');
